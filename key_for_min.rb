2f5d39fd6b14c7a6d1c4df54f78a6cf00c7611cb
HANA LEANDER <hana0070@gmail.com>
2019-04-21 23:08:47 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,8 +3,8 @@
 
 def key_for_min_value(name_hash)
   lowest_key = nil
-  lowest_value = nil 
-  name_hash.collect do |symbol, value|
+  lowest_value = nil
+  name_hash.each do |symbol, value|
 
   end
    key
